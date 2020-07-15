74e46caca3545afd29f857eb741b9c3261396a0b
Michael Beebower <michael.beebower@g.austincc.edu>
2020-07-15 08:20:36 -0500

    Created React component properties in src/index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Hello() {
+function Hello(props) {
+    console.log(Object.keys(props));    // will display an object array of keys of the props
     return (
         <div>
-            <h1>Welcome to React!</h1>
-            <p>Let's build something cool.</p>
+            <h1>Welcome to { props.library }!</h1>
+            <p>{ props.message }</p>
+            <p>{ Object.keys(props).length } Props Total</p>
         </div>
     );
 }
 
 ReactDOM.render(
-    <Hello />,
+    <Hello
+        library="React"
+        message="Have fun!"
+        number={3}
+    />,
     document.getElementById('root')
 );
